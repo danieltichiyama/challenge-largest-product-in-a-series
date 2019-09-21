6d8832c7807583e34742f637316cee61b22698f9
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-21 09:56:33 -1000

    finished, ready for review

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,30 @@
 class LargestProduct {
   constructor(number) {
+    this.number = number;
+  }
 
+  getLargestProduct() {
+    let arr = ("" + this.number).split("");
+    return arr.reduce((accumulator, current, i, array) => {
+      let product =
+        current * array[i + 1] * array[i + 2] * array[i + 3] * array[i + 4];
+      if (product > accumulator) {
+        return product;
+      } else {
+        return accumulator;
+      }
+    }, 0);
+  }
+
+  getLargestProductIndex() {
+    let arr = ("" + this.number).split("");
+    let largestProduct = this.getLargestProduct();
+    for (let i = 0; i < arr.length; i++) {
+      let product = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4];
+      if (product === largestProduct) {
+        return i;
+      }
+    }
   }
 }
 
