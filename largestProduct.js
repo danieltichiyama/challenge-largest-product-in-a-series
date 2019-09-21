9efdab357a909705c402f684ffe667c3cdfdd341
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-21 10:11:06 -1000

    moved methods into constructor

--- largestProduct.js
+++ largestProduct.js
@@ -1,30 +1,29 @@
 class LargestProduct {
   constructor(number) {
-    this.number = number;
-  }
+    this.getLargestProduct = function() {
+      let arr = ("" + number).split("");
+      return arr.reduce((accumulator, current, i, array) => {
+        let product =
+          current * array[i + 1] * array[i + 2] * array[i + 3] * array[i + 4];
+        if (product > accumulator) {
+          return product;
+        } else {
+          return accumulator;
+        }
+      }, 0);
+    };
 
-  getLargestProduct() {
-    let arr = ("" + this.number).split("");
-    return arr.reduce((accumulator, current, i, array) => {
-      let product =
-        current * array[i + 1] * array[i + 2] * array[i + 3] * array[i + 4];
-      if (product > accumulator) {
-        return product;
-      } else {
-        return accumulator;
+    this.getLargestProductIndex = function() {
+      let arr = ("" + number).split("");
+      let largestProduct = this.getLargestProduct();
+      for (let i = 0; i < arr.length; i++) {
+        let product =
+          arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4];
+        if (product === largestProduct) {
+          return i;
+        }
       }
-    }, 0);
-  }
-
-  getLargestProductIndex() {
-    let arr = ("" + this.number).split("");
-    let largestProduct = this.getLargestProduct();
-    for (let i = 0; i < arr.length; i++) {
-      let product = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4];
-      if (product === largestProduct) {
-        return i;
-      }
-    }
+    };
   }
 }
 
